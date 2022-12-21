c0d1bcf1d4f6c073d5415e87dc6432f5eefcb61a
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2022-12-21 15:36:33 +0100

    documentation for crate 0.2.0

---
+++ doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"lib":{"doc":"Modules of the project","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ doc/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"lib":["",[["coordinates",[],["coordinate.rs","gen_coordinate.rs","mod.rs","safe_coordinate.rs","unsafe_coordinate.rs"]]],["lib.rs"]]\
+}');
+createSourceSidebar();
