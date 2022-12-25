9a60bbbef1948bbd8811d18ce283de47755ee06d
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2022-12-25 01:10:09 +0100

    Correction duplicate code

--- src/graphs/polygon_unit.rs
+++ src/graphs/polygon_unit.rs
@@ -8,25 +8,7 @@ pub enum Polygon<T> {
 }
 
 impl<T> Polygon<T> {
-    pub fn new() -> Self {
-        Polygon::PVec(PVec::new())
-    }
 
-    pub fn add_point_single(&mut self, x: T, y: T) {
-        // add point to polygon
-    }
-}
-use PVec;
-
-pub enum Polygon<T>{
-    PVec(PVec<T>),
-    //Array
-    //LnkList
-    //BHeap 
-}
-
-
-impl Polygon{
     pub fn new() -> Self{
         Polygon::PVec(PVec::new());
     }
