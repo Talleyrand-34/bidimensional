a7ac76c8c994bf719c6993a7947041f52a263d30
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2022-12-25 01:07:32 +0100

    Get array of ECoordinates

--- src/graphs/polygon_unit.rs
+++ src/graphs/polygon_unit.rs
@@ -31,6 +31,9 @@ impl Polygon{
         Polygon::PVec(PVec::new());
     }
 
+    pub fn new_vec() -> Self{
+        Polygon::PVec(PVec::new());
+    }
 
     pub fn add_point_single(&mut self, x:T, y:T) {
         match self{
@@ -39,6 +42,22 @@ impl Polygon{
             _ => unimplemented!(),
         }
     }
+
+    pub fn get_array(&self) -> &[ECoordinate<T>] {
+        match self {
+            Polygon::PVec(p) => p.Vec.as_slice(),
+            _ => unimplemented!(),
+        }
+    }
+
+
+    /*pub fn get_coordinates(&self){
+        match self{
+            Polygon::PVec(p)=>{
+
+            }
+
+    }*/
 
 
 }
